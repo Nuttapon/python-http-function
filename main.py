ddab459263d86264118fa1f4e1dabae8feeb9081
Nuttapon Achachotipong <nuttapon.a@linecorp.com>
2023-11-09 21:58:13 +0700

    feat: add Pipfile to manage Python dependencies and specify Python version feat(main.py): modify greeting message to include 'from Cloud Functions' for better context understanding

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def hello_http(request):
         name = request_args["name"]
     else:
         name = "World"
-    return f"Hello {escape(name)}!"
+    return f"Hello {escape(name)}! from Cloud Functions"
