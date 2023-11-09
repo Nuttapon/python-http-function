d50e0e31f62d477d5eadb72d0ed5fd1467a55a70
Nuttapon Achachotipong <nuttapon.a@linecorp.com>
2023-11-09 23:20:29 +0700

    feat(main.py): update return message in hello_http function for testing deployment 2

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def hello_http(request):
         name = request_args["name"]
     else:
         name = "World"
-    return f"Hello {escape(name)}! from Cloud Functions"
+    return f"Hello {escape(name)}! Test deploy 2"
