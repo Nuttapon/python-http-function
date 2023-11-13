ff9c33d0a747b9e0c48b953be02c16731f60a8c7
Nuttapon Achachotipong <nuttapon.a@linecorp.com>
2023-11-13 19:16:02 +0700

    feat(.env): add REGION environment variable to specify deployment region feat(staging-deployment.yml): add REGION to set-env-vars to use in deployment feat(main.py): use REGION environment variable in hello_http response to display deployment region

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import escape
+import os
 
 import functions_framework
 
@@ -22,4 +23,4 @@ def hello_http(request):
         name = request_args["name"]
     else:
         name = "World"
-    return f"Hello {escape(name)}! Test deploy from github action with new service account"
+    return f"Hello {escape(name)}! Test deploy from github action with new service account {os.environ['REGION']}"
